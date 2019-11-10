59449ac92420f91cc676f6edc8069419b1b3a01f
Federica Pelzel <fedepelzel@gmail.com>
2019-11-10 14:27:02 -0500

    adding structure

--- src/DataAnalysis.java
+++ src/DataAnalysis.java
@@ -1,4 +1,24 @@
+/*
+ * This class takes in Tweet objects and analyzes them for sentiment
+ * in different ways
+ */
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class DataAnalysis {
 
+	ArrayList<Tweet> tweets;
+
+	public DataAnalysis(ArrayList<Tweet> tweets) {
+		this.tweets = tweets;
+		}
+	
+	
+
+
+
+
 }
+
+
