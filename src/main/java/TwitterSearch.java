1a62e5878cac7b61a9f83bb0e6197fdc625f3d61
juanfranciscog <juangole@seas.upenn.edu>
2019-11-20 15:17:22 -0500

    Updated comments

--- src/main/java/TwitterSearch.java
+++ src/main/java/TwitterSearch.java
@@ -43,47 +43,52 @@ public class TwitterSearch {
 	ConfigurationBuilder cf = new ConfigurationBuilder().setTweetModeExtended(true);
 	Twitter twitter = TwitterFactory.getSingleton();
 	TwitterFactory tf = new TwitterFactory(cf.build());
-	ArrayList<Tweet> queryResult = new ArrayList<>(); // Stores tweet objects as elements
+	ArrayList<Tweet> queryResult = new ArrayList<>(); // Stores tweet objects as elements of an array
 	long lastTweetMaxId = -1; // tracks the latest tweet retrieved
-	final int MAXSEARCHREQUESTS = 360;
+	final int MAXSEARCHREQUESTS = 180; // Number of requests to be sent (API allows up to 180 requests every 15
+	// minutes
 
 	// Runs the query for the candidate and the date range
-	Query query = new Query(candidate);
-	query.setSince(sinceDate(date));
-	query.setUntil(toDate(date)); 
-	query.count(100);//Number of tweet to be retrieved in each search
-	query.lang("en");//Search for tweets in English
+	Query query = new Query(candidate); // Search term
+	query.setSince(sinceDate(date)); // Lower limit date for the search
+	query.setUntil(toDate(date)); // Upper limit date for the search
+	query.count(100);// Number of tweet to be retrieved in each request (max 100)
+	query.lang("en");// Limits search to tweets in English
 	QueryResult result;
 
-	// run the search as many times as set in MAXSEARCHREQUESTS
+	// Run the search as many times as set in MAXSEARCHREQUESTS
 	for (int numberOfQueries = 0; numberOfQueries < MAXSEARCHREQUESTS; numberOfQueries++) {
-	    // Sets the last tweet ID retrieved
+
+	    // Sets the last tweet ID retrieved, so we retrieve a different batch of tweets
 	    if (lastTweetMaxId != -1) {
 		query.setMaxId(lastTweetMaxId - 1);
 	    }
 
+	    // Executes the search
 	    try {
 		result = twitter.search(query);
-		
-		System.out.println(result.getRateLimitStatus().getRemaining());// Shows how many searches are left and
-		// how long we need to wait to run a new call
-		
-		// Checks if there are still any tweets to retrieve
+		System.out.println(result.getRateLimitStatus().getRemaining());// Shows how many searches are left
+		// before we have to wait
+
+		// Checks if there are still any tweets to retrieve or if we don't have any
+		// requests left
 		if (result.getTweets().size() == 0 || result.getRateLimitStatus().getRemaining() == 0) {
 		    System.out.println("No more tweets left to retrieve.");
 		    return queryResult;
 		}
-		
-		
+
+		// If tweet has extended text, retrieves text over 140 characters
 		for (Status status : result.getTweets()) {
 		    String text = status.getRetweetedStatus() != null ? status.getRetweetedStatus().getText()
 			    : status.getText();
-		    // Added if condition to check the candidate name is in the main text and excludes retweets
+		    // Added if condition to check the candidate name is in the main text and to
+		    // excludes retweets
 		    if (text.contains(candidate) && !status.isRetweet()) {
 			// Updates the highest ID in the tweets retrieved
 			if (lastTweetMaxId == -1 || status.getId() < lastTweetMaxId) {
 			    lastTweetMaxId = status.getId();
 			}
+			// Populates the tweet object witht he search result
 			Tweet tw = new Tweet(status.getId(), status.getUser(), status.getUser().getFollowersCount(),
 				status.getUser().getLocation(), text, status.getCreatedAt(), candidate, 0,
 				status.getRetweetCount(), status.isRetweet(), status.getGeoLocation());
@@ -94,7 +99,8 @@ public class TwitterSearch {
 	    } catch (TwitterException e) {
 		// TODO Auto-generated catch block
 		if (e.exceededRateLimitation()) {
-		    System.out.println("Rate of searchs exceeded. Please wait " + e.getRateLimitStatus().getSecondsUntilReset() + " seconds and try again.");
+		    System.out.println("Rate of searchs exceeded. Please wait "
+			    + e.getRateLimitStatus().getSecondsUntilReset() + " seconds and try again.");
 		    break;
 		}
 	    } catch (NullPointerException f) {
