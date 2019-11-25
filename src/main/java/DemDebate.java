effafc4e13f013aaf8e7ee25f513a15af1c25cbe
Federica Pelzel <fedepelzel@gmail.com>
2019-11-24 22:23:15 -0500

    updates to test file and debate runner

--- src/main/java/DemDebate.java
+++ src/main/java/DemDebate.java
@@ -6,33 +6,37 @@ import edu.stanford.nlp.util.CoreMap;
 public class DemDebate {
 
 	public static void main(String[] args) {
-		// Create a twitter search for each candidate at the debate date
-		// Need to adjust to consume from file
-//		TwitterSearch tsWarren = new TwitterSearch("Warren", "20191120");
-//		TwitterSearch tsSanders = new TwitterSearch("Sanders", "20191120");
-//		TwitterSearch tsBiden = new TwitterSearch("Biden", "20191120");
-//		TwitterSearch tsPete = new TwitterSearch("Buttigieg", "20191120");
-
-//		ArrayList<Tweet> result = tsWarren.mainSearch();
-
-//		System.out.println(result);
 
 		ArrayList<Tweet> tweets = SaveTweets.loadFile();
 		System.out.println(tweets.size());
 		for (int i = 0; i < tweets.size(); i++) {
 			Tweet tweet = tweets.get(i);
-//			System.out.println(tweet.getCandidate());
-//			System.out.println(tweet.getId());
-//			System.out.println(tweet.getTweetDate());
-//			System.out.println(tweet.getInfluenceScore());
-//			System.out.println(tweet.getNumberOfFollowers());
-//			System.out.println(tweet.getLocation());
-//			System.out.println(tweet.getTextInTweet());
-//			System.out.println(tweet.getIsRetweet());
-//			System.out.println(tweet.getLocation());
-
+		}
+		
+		ArrayList<Tweet> warren = new ArrayList<>();
+		ArrayList<Tweet> biden = new ArrayList<>();
+		ArrayList<Tweet> sanders = new ArrayList<>();
+		ArrayList<Tweet> pete = new ArrayList<>();
+				
+		for(Tweet t : tweets) {
+			if (t.getCandidate().equals("Warren")) {
+				warren.add(t);
+			}
+			if (t.getCandidate().equals("Sanders")) {
+				sanders.add(t);
+			}
+			if (t.getCandidate().equals("Buttigieg")) {
+				pete.add(t);
+			}
+			if (t.getCandidate().equals("Biden")) {
+				biden.add(t);
+			}
 		}
 
+		/*
+		 * Analysis of all tweets
+		 */
+		
 		NLPAnalyser nlp = new NLPAnalyser();
 		int current = 0;
 		for (Tweet tweet : tweets) {
@@ -59,21 +63,34 @@ public class DemDebate {
 		System.out.println(da.sentimentState(tbs));
 		System.out.println(da.mostUsedWords());
 
-		// run analysis
-		// print output
+		/*
+		 * Analysis specific to Elizabeth Warren
+		 */
+		
+		NLPAnalyser nlpWarren = new NLPAnalyser();
+		int currentWarren = 0;
+		for (Tweet tweet : warren) {
+			List<CoreMap> sentences = nlp.nlpPipeline(tweet.getTextInTweet());
+			double sentimentScore = nlp.getSentimentScore(sentences);
+			tweet.setSentimentScore(sentimentScore);
+			HashMap<String, Double> as = nlp.adjectivesScoring(sentences);
+			tweet.setAdjSentiment(as);
+			System.out.println(currentWarren + "/" + tweets.size() + "; score=" + sentimentScore + "; adj =" + as);
+			currentWarren += 1;
+		}
+		
+		TweetsByState tbsWarren = new TweetsByState(warren);
+		System.out.println(tbsWarren.states.get("DC").size());
+		int countWarren = 0;
+		for (String state : tbsWarren.states.keySet()) {
+			System.out.println(state + " " + tbsWarren.states.get(state).size());
+			count += tbsWarren.states.get(state).size();
+		}
 
-		// Create a csv with total tweets per candidate per day or hour (Date, Time?,
-		// Candidate, number of tweets)
-		// Create a csv with average sentiment per candidate per day or hour (Date,
-		// Time?, Candidate, average sentiment)
-		// Create a csv with sentiment per state before debate (Candidate, State,
-		// Average Sentiment)
-		// Create a csv with sentiment per state after debate (Candidate, State, Average
-		// Sentiment)
-		// Create a csv with most used adjectives per candidate (Candidate, word, number
-		// of times seen, sentiment score)
-		// return most changed in average sentiment before and after
-		// return followers gained for each candidate
+		System.out.println(countWarren);
 
+		DataAnalysis daWarren = new DataAnalysis(warren);
+		System.out.println(daWarren.sentimentState(tbsWarren));
+		System.out.println(daWarren.mostUsedWords());
 	}
 }
