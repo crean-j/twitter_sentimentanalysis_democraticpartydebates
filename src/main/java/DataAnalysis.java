e90319330f96336ca1ff36b9796f756e8c70fc28
juanfranciscog <juangole@seas.upenn.edu>
2019-11-26 21:41:08 -0500

    Revert "Merge branch 'master' of https://github.com/UPenn-CIT599/final-project-twitterdemocraticpartydebates"
    This reverts commit f1611a09a5d8c32719ec87782e3284eb68c781d3, reversing
    changes made to b383d84b7794156e90f0188c88814f7133683359.

--- src/main/java/DataAnalysis.java
+++ src/main/java/DataAnalysis.java
@@ -1,3 +1,4 @@
+package main.java;
 
 /*
  * This class takes in Tweet objects and analyzes them for sentiment
@@ -6,10 +7,7 @@
  */
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 public class DataAnalysis {
 
@@ -35,54 +33,40 @@ public class DataAnalysis {
 	 * sentimentScore. loops through all tweets and returns the average sentiment
 	 * score for the keyword
 	 */
-
-	public double sentimentScore() {
-
+	public String sentimentScore() {
 		double total = 0.0;
 		for (Tweet t : tweets) {
 			total += t.getSentimentScore();
 		}
-		return total / tweets.size();
+		return "The sentiment score is: " + total / tweets.size() + "/4";
 	}
 
 	/**
 	 * mostUsedWords method Returns the most seen adjectives used in tweets
-	 * mentioning the keyword. Returns a HashMap with word + number of times
-	 * mentioned
-	 * 
+	 * mentioning the keyword Ideally return with the key and value (for example: 1.
+	 * Brilliant, mentioned 234 times.
 	 */
-	public HashMap<String, Integer> mostUsedWords() {
-		HashMap<String, Integer> adjectivesCount = new HashMap<String, Integer>();
+	public String mostUsedWords(int numberOfResults) {
+		String topWords = null;
+		HashMap<String, Integer> mostUsedAdjectives = new HashMap<String, Integer>();
 		for (Tweet t : tweets) {
-			for (String adj : t.getAdjSentiment().keySet()) {
-				if (!adjectivesCount.containsKey(adj)) {
-					adjectivesCount.put(adj, 0);
-				}
-				adjectivesCount.put(adj, adjectivesCount.get(adj) +1);
-				
-			}
+			// for each tweet count adjectives used and add to HashMap
 		}
-
-		// sort hashmap using Comparator
-		LinkedHashMap<String, Integer> adjectivesSorted = new LinkedHashMap<>();
-		adjectivesCount.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-				.forEachOrdered(x -> adjectivesSorted.put(x.getKey(), x.getValue()));
-
-		return adjectivesSorted;
+		return topWords;
 	}
 
 	/**
-	 * numberOfTweets method returns a HashMap with Key State and value number of
-	 * tweets originating from that state Uses the TweetsByState class
+	 * topLocations method returns a String with the number of tweets per location for 
+	 * a variable number of results
 	 */
-	public HashMap<String, Integer> numberOfTweetsState() {
-		HashMap<String, Integer> stateTweets = new HashMap<String, Integer>();
-		TweetsByState tbs = new TweetsByState(tweets);
-		for (String state : tbs.states.keySet()) {
-			stateTweets.put(state, tbs.states.values().size());
-			;
+	public String topLocations(int numberOfResults) {
+		String topStates = null;
+		HashMap<String, Integer> location = new HashMap<String, Integer>();
+		for (Tweet t : tweets) {
+			// count how many tweets per location
+			// may need to do some data modifications to just take in states
 		}
-		return stateTweets;
+		return topStates; // ideally return with the key and value (for example: 1. CA, 45000 tweets.
 	}
 
 	/**
@@ -95,51 +79,29 @@ public class DataAnalysis {
 				mostRetweeted = t;
 			}
 		}
-		return mostRetweeted.getUser() + ": " + mostRetweeted.getTextInTweet() + "retweeted: "
-				+ mostRetweeted.getRetweetedCount() + " times."; // to be formatted
+		return mostRetweeted.getUser() + ": "+ mostRetweeted.getTextInTweet() + "retweeted: " + mostRetweeted.getRetweetedCount() + " times."; // to be formatted
 	}
 
 	/**
 	 * sentiment by state. Create a Hashmap with the average sentiment by state
 	 * could return most postive or negative states.
-	 * 
-	 * Issues to solve: returns same average score for each state
 	 */
-	public HashMap<String, Double> sentimentState(TweetsByState tbs2) {
-		HashMap<String, Double> sentState = new HashMap<String, Double>();
-
-		TweetsByState tbs = new TweetsByState(tweets);
-		for (String state : tbs.states.keySet()) {
-			
-				double totalSent = 0.0;
-				int count = 0;
-				
-				for (Tweet t : tbs.states.get(state)) {
-					totalSent += t.getSentimentScore();
-					count += 1;
-				}
-				
-				if(count > 0) {
-				double average = totalSent / count;
-				sentState.put(state, average);
-				}
-		}
-		
-		LinkedHashMap<String, Double> sentStateSorted = new LinkedHashMap<>();
-		sentState.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-				.forEachOrdered(x -> sentStateSorted.put(x.getKey(), x.getValue()));
-
-		return sentStateSorted;
+	public String sentimentState() {
+		String sentiment = null;
+		// hashmap with key state and tweets
+		// average sentiment by state?
+		// need to figure out how to tackle this one
+		return sentiment;
 	}
 
 	/**
 	 * Method gets the average sentiment one week before a certain date
 	 */
-	public int averageSevenDBefore(String date) {
+	public int averageSevenDBefore(String date){
 		int average = 0;
 
-		// loops through array list of tweets
-		// gets average sentiment for tweets one week before date
+		//loops through array list of tweets
+		//gets average sentiment for tweets one week before date
 
 		return average;
 	}
@@ -147,11 +109,11 @@ public class DataAnalysis {
 	/**
 	 * Method gets the average sentiment one week after a certain date
 	 */
-	public int averageSevenDAfter(String date) {
+	public int averageSevenDAfter(String date){
 		int average = 0;
 
-		// loops through array list of tweets
-		// gets average sentiment for tweets one week after date
+		//loops through array list of tweets
+		//gets average sentiment for tweets one week after date
 
 		return average;
 	}
--- src/main/java/SaveTweets.java
+++ src/main/java/SaveTweets.java
@@ -8,8 +8,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Scanner;
-import twitter4j.GeoLocation;
-import twitter4j.User;
 import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -20,135 +18,68 @@ import java.text.SimpleDateFormat;
  */
 
 public class SaveTweets {
-	ArrayList<Tweet> queryResult = new ArrayList<>(); // Stores the result of latest search
-	ArrayList<String> tweetsAlreadyStored = new ArrayList<>(); // Stores the tweet that are saved in the txt file
-	Timestamp timestamp = new Timestamp(System.currentTimeMillis()); // Creates a new timestamp
-	static private String filename = "TweetArchive.txt";
+    ArrayList<Tweet> queryResult = new ArrayList<>(); // Stores the result of latest search
+    ArrayList<String> tweetsAlreadyStored = new ArrayList<>(); // Stores the tweet that are saved in the txt file
+    Timestamp timestamp = new Timestamp(System.currentTimeMillis()); // Creates a new timestamp
 
-	/**
-	 * Constructor
-	 * 
-	 * @param queryResult the result of the search
-	 */
-	public SaveTweets(ArrayList<Tweet> queryResult) {
-		this.queryResult = queryResult;
-	}
+    /**
+     * Constructor
+     * 
+     * @param queryResult the result of the search
+     */
+    public SaveTweets(ArrayList<Tweet> queryResult) {
+	this.queryResult = queryResult;
+    }
 
-	/**
-	 * Loads the tweets from the TweetArchive file, then compares the tweet id from
-	 * the last search and if it can't find it in the TweetArchive file, it adds the
-	 * whole tweet to the file.
-	 */
-	public void saveToFile() {
-		// read file with save tweets into an ArrayList and stores the tweet ID
-		try {
-			Scanner in;
-			File myFile = new File(filename);
-			in = new Scanner(myFile);
-			while (in.hasNextLine()) {
-				String[] lineInFile = in.nextLine().split("@@@");
-				tweetsAlreadyStored.add(lineInFile[0]);
-			}
-		} catch (FileNotFoundException e1) {
-			// TODO Auto-generated catch block
-			try {
-				FileWriter fw;
-				fw = new FileWriter(filename, true);
-				PrintWriter pw = new PrintWriter(fw);
-				pw.flush();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			e1.printStackTrace();
-
-		}
-
-		// Loops the result of the last search and adds to the txt file the tweets that
-		// are not already there
-		for (Tweet tw : queryResult) {
-			if (!tweetsAlreadyStored.contains(String.valueOf(tw.getId()))) {
-				try {
-					FileWriter fw = new FileWriter(filename, true);
-					PrintWriter pw = new PrintWriter(fw);
-					Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-					pw.println(tw.getId() + "@@@" + timestamp + "@@@" + tw.getCandidate() + "@@@"
-							+ tw.getUser().getName() + "@@@" + tw.getUser().getLocation() + "@@@"
-							+ tw.getNumberOfFollowers() + "@@@" + tw.getTextInTweet().replaceAll("\\n*", "") + "@@@"
-							+ tw.getLocation() + "@@@" + tw.getRetweetedCount() + "@@@" + tw.getInfluenceScore() + "@@@"
-							+ tw.getTweetDate() + "@@@" + tw.getSentimentScore() + "@@@" + tw.isRetweet() + "@@@"
-							+ tw.getGeoLocation());
-					pw.flush();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					System.out.println("Error writing to the file.");
-					e.printStackTrace();
-				}
-
-			}
-		}
-	}
-
-	/*
-	 * author: Federica Pelzel the loadfile method reads in a csv with tweets and
-	 * adds them to an ArrayList of Tweets
-	 */
-
-	static public ArrayList<Tweet> loadFile() {
-
-		Scanner in;
-		ArrayList<Tweet> tweets = new ArrayList<Tweet>();
-		// read tweets file and add to array list
-		try {
-			in = new Scanner(new FileReader(filename));
-
-			// read column headers
-			while (in.hasNextLine()) {
-				Tweet parsed = tweetsParse(in.nextLine());
-				if (parsed != null) {
-					tweets.add(parsed);
-				}
-			}
-
-			in.close();
-
-		} catch (FileNotFoundException e) {
-			System.out.println(e.getMessage());
-			e.printStackTrace();
-		}
-		return tweets;
+    /**
+     * Loads the tweets from the TweetArchive file, then compares the tweet id from
+     * the last search and if it can't find it in the TweetArchive file, it adds the
+     * whole tweet to the file.
+     */
+    public void saveToFile() {
+	// read file with save tweets into an ArrayList and stores the tweet ID
+	try {
+	    Scanner in;
+	    File myFile = new File("TweetArchive.txt");
+	    in = new Scanner(myFile);
+	    while (in.hasNextLine()) {
+		String[] lineInFile = in.nextLine().split("@@@");
+		tweetsAlreadyStored.add(lineInFile[0]);
+	    }
+	} catch (FileNotFoundException e1) {
+	    // TODO Auto-generated catch block
+	    try {
+		FileWriter fw;
+		fw = new FileWriter("TweetArchive.txt", true);
+		PrintWriter pw = new PrintWriter(fw);
+		pw.flush();
+	    } catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	    }
+	    e1.printStackTrace();
 
 	}
 
-	/*
-	 * author: Federica Pelzel the tweetsParse method parses each row in the file an
-	 * creates Tweet Objects from them
-	 */
-	static public Tweet tweetsParse(String row) {
-
-		String[] rowArr = row.split("@@@");
+	// Loops the result of the last search and adds to the txt file the tweets that
+	// are not already there
+	for (Tweet tw : queryResult) {
+	    if (!tweetsAlreadyStored.contains(String.valueOf(tw.getId()))) {
 		try {
-			long id = rowArr[0].length() == 0 ? -1 : Long.parseLong(rowArr[0]);
-			int numberOfFollowers = rowArr[5].length() == 0 ? -1 : Integer.parseInt(rowArr[5]);
-			String location = rowArr[4];
-			String textInTweet = rowArr[6];
-			SimpleDateFormat simpleDate = new SimpleDateFormat("EEE MMM d HH:mm:ss z YYYY");
-			Date tweetDate = null;
-			tweetDate = simpleDate.parse(rowArr[10]);
-			String candidate = rowArr[2];
-			double influenceScore = rowArr[9].length() == 0 ? -1 : Double.parseDouble(rowArr[8]);
-			int retweetedCount = rowArr[8].length() == 0 ? -1 : Integer.parseInt(rowArr[8]);
-			double sentimentScore = rowArr[11].length() == 0 ? -1 : Double.parseDouble(rowArr[11]);
-			boolean isRetweet = rowArr[12].equals("true");
-
-			Tweet t = new Tweet(id, null, numberOfFollowers, location, textInTweet, tweetDate, candidate,
-					influenceScore, retweetedCount, isRetweet, null);
-			
-			return t;
-			
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			return null;
+		    FileWriter fw = new FileWriter("TweetArchive.txt", true);
+		    PrintWriter pw = new PrintWriter(fw);
+		    Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+		    pw.println(tw.getId() + "@@@" + timestamp + "@@@" + tw.getCandidate() + "@@@"
+			    + tw.getUser().getName() + "@@@" + tw.getUser().getLocation() + "@@@"
+			    + tw.getNumberOfFollowers() + "@@@" + tw.getTextInTweet().replaceAll("\\n*", "") + "@@@"
+			    + tw.getRetweetedCount() + "@@@" + tw.getInfluenceScore() + "@@@" + tw.getSentimentScore());
+		    pw.flush();
+		} catch (IOException e) {
+		    // TODO Auto-generated catch block
+		    System.out.println("Error writing to the file.");
+		    e.printStackTrace();
 		}
+
+	    }
 	}
 }
