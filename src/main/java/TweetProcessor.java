209b63fbd6cd1ff3fa8cdccccb8811dfb9384da1
Joanne Crean <creanj@seas.upenn.edu>
2019-12-05 21:30:49 +0000

    cleaned up tweet processor class

--- src/main/java/TweetProcessor.java
+++ src/main/java/TweetProcessor.java
@@ -1,14 +1,7 @@
-//package main.java;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.Arrays;
-
 /**
  * Class takes tweet text and cleans it up for natural language processing
  * It includes methods to:
- * Lowercase the text
- * Remove noise from text
+ * Remove noise from text: urls, users mentions and hash-tags
  *
  * @author joannecrean
  */
@@ -21,7 +14,9 @@ public class TweetProcessor {
     public TweetProcessor() { }
 
     /**
-     * Method removes whitespace and makes the text lower case
+     * Method to do additional clean-up of tweets
+     * Method is optional if sentiment score returned is being influenced by:
+     * casing, whitespace, capitalisation
      * @return String of text
      */
     public String cleanText(String text) {
@@ -29,8 +24,12 @@ public class TweetProcessor {
         // remove whitespace
         String trimText = text.trim();
 
+        //remove punctuation
+        trimText = trimText.replaceAll("\\p{Punct}+", "");
+
         // to lowercase
         return trimText.toLowerCase();
+
     }
 
     /**
@@ -38,11 +37,7 @@ public class TweetProcessor {
      * @return String of text
      */
     public String removeNoise(String text) {
-        String finalText = text;
-        String tweet = text;
-
-        //convert tweet to lower case
-        //tweet = tweet.toLowerCase();
+        String tweet = text; // processed text
 
         //remove urls
         tweet = tweet.replaceAll("((www\\.[^\\s]+)|(https?://[^\\s]+))", "");
@@ -53,20 +48,6 @@ public class TweetProcessor {
         //remove # from hash tag
         tweet = tweet.replaceAll("#", "");
 
-        //remove punctuation
-        //tweet = tweet.replaceAll("\\p{Punct}+", "");
-
-        //System.out.println(tweet);
         return tweet;
     }
-
-
-    /*public static void main(String[] args) {
-    	TweetProcessor tp = new TweetProcessor();
-        String test = "RT This made my day; glad @JeremyKappell is standing up against #ROC’s disgusting mayor. "
-        		+ "Former TV meteorologist Jeremy Kappell suing Mayor Lovely Warren"
-        		+ "https://t.co/rJIV5SN9vB (Via NEWS 8 WROC)";
-        String finalText = tp.removeNoise(test);
-        System.out.println(finalText);
-    }*/
 }
