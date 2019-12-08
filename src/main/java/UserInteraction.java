1adc041299fcb95edbb36b3f611369d5ded28e63
Joanne Crean <creanj@seas.upenn.edu>
2019-12-08 12:43:59 +0000

    code format

--- src/main/java/UserInteraction.java
+++ src/main/java/UserInteraction.java
@@ -6,42 +6,41 @@ import java.util.Scanner;
  * entered
  *
  * @author juangoleniowski
- *
  */
 public class UserInteraction {
     private String candidate; // Candidate to be searched
 
     // Constructor
     public UserInteraction() {
-	selectSearchTerm();
+        selectSearchTerm();
     }
 
     /**
-     * 
      * Asks user to enter a search term.
      */
     private void selectSearchTerm() {
-	Scanner scanner = new Scanner(System.in);
-	System.out.println("\tEnter a word to search and do a sentiment analysis: ");
-	candidate = scanner.nextLine();
-	System.out.println("\t\t\t *****\tYou have selected " + candidate + "\t*****\n");
-	System.out.println(
-		"\t We are going to search for tweets starting from today's date and go back as far as possible, up to seven days");
-	scanner.close();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a word to search and do a sentiment analysis: ");
+        candidate = scanner.nextLine();
+        System.out.println("\t\t\t *****\tYou have selected " + candidate + "\t*****\n");
+        System.out.println(
+                "We are going to search for tweets starting from today's date and go back as far as possible, " +
+                        "up to seven days");
+        scanner.close();
     }
 
     /**
      * @return the date in format YYYY-MM-DD
      */
     public String getDate() {
-	Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-	return ("" + timestamp).substring(0, 11);
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        return ("" + timestamp).substring(0, 11);
     }
 
     /**
      * @return the candidate
      */
     public String getCandidate() {
-	return candidate;
+        return candidate;
     }
 }
