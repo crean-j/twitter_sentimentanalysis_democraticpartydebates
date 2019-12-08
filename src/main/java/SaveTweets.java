89cdefd8091e37ddebef98b32bd78d8002b2aa83
Joanne Crean <creanj@seas.upenn.edu>
2019-12-08 12:52:08 +0000

    formatted code

--- src/main/java/SaveTweets.java
+++ src/main/java/SaveTweets.java
@@ -10,9 +10,12 @@ import java.util.Scanner;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 
-/*
- * 
- * This class saves tweets to a text file as we can only go back 7 days in history
+/**
+ * This class saves tweets to a text file
+ * Used to build historical archive for static analysis, one file was continuously updated
+ * Used to save real time search results to a new file each time the program is run
+ *
+ * @author juangoleniowski, Federica Pelzel
  */
 
 public class SaveTweets {
@@ -21,135 +24,139 @@ public class SaveTweets {
     // Date stamp and file name to save the tweets
     static SimpleDateFormat dateFormat = new SimpleDateFormat("dd_MM_yyyy-HH_mm_ss");
     static Date dateForFile = new Date();
-    // static private Timestamp timestamp = new
     // Timestamp(System.currentTimeMillis()); // Creates a new timestamp
     private static String filename = "Tweet_Analysis_" + dateFormat.format(dateForFile) + ".txt";
 
     /**
      * Constructor
-     * 
+     *
      * @param queryResult the result of the search
      */
     public SaveTweets(ArrayList<Tweet> queryResult) {
-	this.queryResult = queryResult;
+        this.queryResult = queryResult;
     }
 
     /**
      * Loads the tweets from the TweetArchive file, then compares the tweet id from
      * the last search and if it can't find it in the TweetArchive file, it adds the
      * whole tweet to the file.
-     * 
      */
     public void saveToFile() {
-	// reads file with saved tweets into an ArrayList and stores the tweet ID. This
-	// section of the code was used when extracting tweets for the static analysis
-	// around the democratic debate on Nov 20th
-	try {
-	    Scanner in;
-	    File myFile = new File(filename);
-	    in = new Scanner(myFile);
-	    while (in.hasNextLine()) {
-		String[] lineInFile = in.nextLine().split("@@@");
-		tweetsAlreadyStored.add(lineInFile[0]);
-	    }
-	    in.close();
-	} catch (FileNotFoundException e1) {
-	    try {
-		FileWriter fw;
-		fw = new FileWriter(filename, true);
-		PrintWriter pw = new PrintWriter(fw);
-		pw.flush();
-		pw.close();
-	    } catch (IOException e) {
-		e.printStackTrace();
-	    }
-	    System.out.println("A file called " + filename + " will be created.");
-	}
+        // reads file with saved tweets into an ArrayList and stores the tweet ID. This
+        // section of the code was used when extracting tweets for the static analysis
+        // around the democratic debate on Nov 20th
+        try {
+            Scanner in;
+            File myFile = new File(filename);
+            in = new Scanner(myFile);
+            while (in.hasNextLine()) {
+                String[] lineInFile = in.nextLine().split("@@@");
+                tweetsAlreadyStored.add(lineInFile[0]);
+            }
+            in.close();
+        } catch (FileNotFoundException e1) {
+            try {
+                FileWriter fw;
+                fw = new FileWriter(filename, true);
+                PrintWriter pw = new PrintWriter(fw);
+                pw.flush();
+                pw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("A file called " + filename + " will be created.");
+        }
 
-	// Loops the result of the last search and adds to the txt file the tweets that
-	// are not already there
-	for (Tweet tw : queryResult) {
-	    if (!tweetsAlreadyStored.contains(String.valueOf(tw.getId()))) {
-		try {
-		    FileWriter fw = new FileWriter(filename, true);
-		    PrintWriter pw = new PrintWriter(fw);
-		    Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-		    pw.println(tw.getId() + "@@@" + timestamp + "@@@" + tw.getCandidate() + "@@@" + tw.getUser() + "@@@"
-			    + tw.getLocation() + "@@@" + tw.getNumberOfFollowers() + "@@@"
-			    + tw.getTextInTweet().replaceAll("\\n*", "") + "@@@" + tw.getLocation() + "@@@"
-			    + tw.getRetweetedCount() + "@@@" + tw.getInfluenceScore() + "@@@" + tw.getTweetDate()
-			    + "@@@" + tw.getSentimentScore() + "@@@" + tw.isRetweet() + "@@@" + tw.getGeoLocation());
-		    pw.flush();
-		    pw.close();
-		} catch (IOException e) {
-		    System.out.println("Error writing to the file.");
-		    e.printStackTrace();
-		}
+        // Loops the result of the last search and adds to the txt file the tweets that
+        // are not already there
+        for (Tweet tw : queryResult) {
+            if (!tweetsAlreadyStored.contains(String.valueOf(tw.getId()))) {
+                try {
+                    FileWriter fw = new FileWriter(filename, true);
+                    PrintWriter pw = new PrintWriter(fw);
+                    Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+                    pw.println(tw.getId() + "@@@" + timestamp + "@@@" + tw.getCandidate() + "@@@" + tw.getUser() + "@@@"
+                            + tw.getLocation() + "@@@" + tw.getNumberOfFollowers() + "@@@"
+                            + tw.getTextInTweet().replaceAll("\\n*", "") + "@@@" + tw.getLocation() + "@@@"
+                            + tw.getRetweetedCount() + "@@@" + tw.getInfluenceScore() + "@@@" + tw.getTweetDate()
+                            + "@@@" + tw.getSentimentScore() + "@@@" + tw.isRetweet() + "@@@" + tw.getGeoLocation());
+                    pw.flush();
+                    pw.close();
+                } catch (IOException e) {
+                    System.out.println("Error writing to the file.");
+                    e.printStackTrace();
+                }
 
-	    }
-	}
+            }
+        }
     }
 
-    /*
-     * author: Federica Pelzel the loadfile method reads in a csv with tweets and
+    /**
+     * The loadfile method reads in a csv with tweets and
      * adds them to an ArrayList of Tweets
+     *
+     * @param fileToRead - name of file
+     * @return an ArrayList of Tweets
      */
 
     static public ArrayList<Tweet> loadFile(String fileToRead) {
 
-	Scanner in;
-	ArrayList<Tweet> tweets = new ArrayList<Tweet>();
-	// read tweets file and add to array list
-	try {
-	    in = new Scanner(new FileReader(fileToRead));
+        Scanner in;
+        ArrayList<Tweet> tweets = new ArrayList<Tweet>();
+        // read tweets file and add to array list
+        try {
+            in = new Scanner(new FileReader(fileToRead));
 
-	    // read column headers
-	    while (in.hasNextLine()) {
-		Tweet parsed = tweetsParse(in.nextLine());
-		if (parsed != null) {
-		    tweets.add(parsed);
-		}
-	    }
+            // read column headers
+            while (in.hasNextLine()) {
+                Tweet parsed = tweetsParse(in.nextLine());
+                if (parsed != null) {
+                    tweets.add(parsed);
+                }
+            }
 
-	    in.close();
+            in.close();
 
-	} catch (FileNotFoundException e) {
-	    System.out.println(e.getMessage());
-	    e.printStackTrace();
-	}
-	return tweets;
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        return tweets;
 
     }
 
-    /*
-     * author: Federica Pelzel the tweetsParse method parses each row in the file an
+    /**
+     * The tweetsParse method parses each row in the file an
      * creates Tweet Objects from them
+     *
+     * @param row
+     * @return Tweet object
      */
     static public Tweet tweetsParse(String row) {
 
-	String[] rowArr = row.split("@@@");
-	try {
-	    long id = rowArr[0].length() == 0 ? -1 : Long.parseLong(rowArr[0]);
-	    String user = rowArr[3];
-	    int numberOfFollowers = rowArr[5].length() == 0 ? -1 : Integer.parseInt(rowArr[5]);
-	    String location = rowArr[4];
-	    String textInTweet = rowArr[6];
-	    SimpleDateFormat simpleDate = new SimpleDateFormat("EEE MMM d HH:mm:ss z YYYY");
-	    Date tweetDate = null;
-	    tweetDate = simpleDate.parse(rowArr[10]);
-	    String candidate = rowArr[2];
-	    double influenceScore = rowArr[9].length() == 0 ? -1 : Double.parseDouble(rowArr[8]);
-	    int retweetedCount = rowArr[8].length() == 0 ? -1 : Integer.parseInt(rowArr[8]);
-	    double sentimentScore = rowArr[11].length() == 0 ? -1 : Double.parseDouble(rowArr[11]);
-	    boolean isRetweet = rowArr[12].equals("true");
+        String[] rowArr = row.split("@@@");
+        try {
+            long id = rowArr[0].length() == 0 ? -1 : Long.parseLong(rowArr[0]);
+            String user = rowArr[3];
+            int numberOfFollowers = rowArr[5].length() == 0 ? -1 : Integer.parseInt(rowArr[5]);
+            String location = rowArr[4];
+            String textInTweet = rowArr[6];
+            SimpleDateFormat simpleDate = new SimpleDateFormat("EEE MMM d HH:mm:ss z YYYY");
+            Date tweetDate = null;
+            tweetDate = simpleDate.parse(rowArr[10]);
+            String candidate = rowArr[2];
+            double influenceScore = rowArr[9].length() == 0 ? -1 : Double.parseDouble(rowArr[8]);
+            int retweetedCount = rowArr[8].length() == 0 ? -1 : Integer.parseInt(rowArr[8]);
+            double sentimentScore = rowArr[11].length() == 0 ? -1 : Double.parseDouble(rowArr[11]);
+            boolean isRetweet = rowArr[12].equals("true");
 
-	    Tweet t = new Tweet(id, user, numberOfFollowers, location, textInTweet, tweetDate, candidate,
-		    influenceScore, retweetedCount, isRetweet, null);
+            Tweet t = new Tweet(id, user, numberOfFollowers, location, textInTweet, tweetDate, candidate,
+                    influenceScore, retweetedCount, isRetweet, null);
 
-	    return t;
+            return t;
 
-	} catch (Exception e) {
-	    return null;
-	}
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
--- src/main/java/TwitterSearch.java
+++ src/main/java/TwitterSearch.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 import twitter4j.Query;
 import twitter4j.QueryResult;
 import twitter4j.Status;
@@ -11,7 +12,6 @@ import twitter4j.conf.ConfigurationBuilder;
  * This class handles the queries to Twitter
  *
  * @author juangoleniowski
- *
  */
 public class TwitterSearch {
     private String candidate;
@@ -21,8 +21,8 @@ public class TwitterSearch {
      * Constructor
      */
     public TwitterSearch(String candidate, String date) {
-	this.candidate = candidate;
-	this.date = date;
+        this.candidate = candidate;
+        this.date = date;
     }
 
     /**
@@ -33,159 +33,155 @@ public class TwitterSearch {
      */
     public ArrayList<Tweet> mainSearch() {
 
-	// Creates Twitter instance
-	ConfigurationBuilder cf = new ConfigurationBuilder().setTweetModeExtended(true);
-	Twitter twitter = TwitterFactory.getSingleton();
-	TwitterFactory tf = new TwitterFactory(cf.build());
-	ArrayList<Tweet> queryResult = new ArrayList<>(); // Stores tweet objects as elements in an array
-	long lastTweetMaxId = -1; // tracks the latest tweet retrieved
-	final int MAXSEARCHREQUESTS = 10; // Number of requests to be sent (API allows up to 180 requests every 15
-	// minutes)
+        // Creates Twitter instance
+        ConfigurationBuilder cf = new ConfigurationBuilder().setTweetModeExtended(true);
+        Twitter twitter = TwitterFactory.getSingleton();
+        TwitterFactory tf = new TwitterFactory(cf.build());
+        ArrayList<Tweet> queryResult = new ArrayList<>(); // Stores tweet objects as elements in an array
+        long lastTweetMaxId = -1; // tracks the latest tweet retrieved
+        final int MAXSEARCHREQUESTS = 10; // Number of requests to be sent (API allows up to 180 requests every 15
+        // minutes)
 
-	// Runs the query for the candidate and the date range
-	Query query = new Query(candidate); // Search term
-	query.setSince(sinceDate(date)); // Lower limit date for the search
-	query.setUntil(toDate(date)); // Upper limit date for the search
-	query.count(100);// Number of tweets to be retrieved in each request (max 100)
-	query.lang("en");// Limits search to tweets in English
-	QueryResult result;
+        // Runs the query for the candidate and the date range
+        Query query = new Query(candidate); // Search term
+        query.setSince(sinceDate(date)); // Lower limit date for the search
+        query.setUntil(toDate(date)); // Upper limit date for the search
+        query.count(100);// Number of tweets to be retrieved in each request (max 100)
+        query.lang("en");// Limits search to tweets in English
+        QueryResult result;
 
-	// Run the search as many times as set in MAXSEARCHREQUESTS
-	for (int numberOfQueries = 0; numberOfQueries < MAXSEARCHREQUESTS; numberOfQueries++) {
+        // Run the search as many times as set in MAXSEARCHREQUESTS
+        for (int numberOfQueries = 0; numberOfQueries < MAXSEARCHREQUESTS; numberOfQueries++) {
 
-	    // Sets the last tweet ID retrieved, so we retrieve a different batch of tweets
-	    // in every search
-	    if (lastTweetMaxId != -1) {
-		query.setMaxId(lastTweetMaxId - 1);
-	    }
+            // Sets the last tweet ID retrieved, so we retrieve a different batch of tweets
+            // in every search
+            if (lastTweetMaxId != -1) {
+                query.setMaxId(lastTweetMaxId - 1);
+            }
 
-	    // Executes the search
-	    try {
-		result = twitter.search(query);
+            // Executes the search
+            try {
+                result = twitter.search(query);
 
-		// Checks if there are still any tweets to retrieve or if we don't have any
-		// requests left
-		if (result.getTweets().size() == 0 || result.getRateLimitStatus().getRemaining() == 0) {
-		    System.out.println("No more tweets left to retrieve.");
-		    return queryResult;
-		}
+                // Checks if there are still any tweets to retrieve or if we don't have any
+                // requests left
+                if (result.getTweets().size() == 0 || result.getRateLimitStatus().getRemaining() == 0) {
+                    System.out.println("No more tweets left to retrieve.");
+                    return queryResult;
+                }
 
-		// If tweet has extended text, retrieves text over 140 characters
-		for (Status status : result.getTweets()) {
-		    String text = status.getRetweetedStatus() != null ? status.getRetweetedStatus().getText()
-			    : status.getText();
-		    // Added condition to check that the candidate name is in the main text and to
-		    // excludes retweets
-		    if (text.contains(candidate) && !status.isRetweet()) {
-			// Updates the highest ID in the tweets retrieved
-			if (lastTweetMaxId == -1 || status.getId() < lastTweetMaxId) {
-			    lastTweetMaxId = status.getId();
-			}
-			// Populates the tweet object with the search result
-			Tweet tw = new Tweet(status.getId(), (String) status.getUser().getName(),
-				status.getUser().getFollowersCount(), status.getUser().getLocation(), text,
-				status.getCreatedAt(), candidate, 0, status.getRetweetCount(), status.isRetweet(),
-				status.getGeoLocation());
-			queryResult.add(tw);
-		    }
-		}
+                // If tweet has extended text, retrieves text over 140 characters
+                for (Status status : result.getTweets()) {
+                    String text = status.getRetweetedStatus() != null ? status.getRetweetedStatus().getText()
+                            : status.getText();
+                    // Added condition to check that the candidate name is in the main text and to
+                    // excludes retweets
+                    if (text.contains(candidate) && !status.isRetweet()) {
+                        // Updates the highest ID in the tweets retrieved
+                        if (lastTweetMaxId == -1 || status.getId() < lastTweetMaxId) {
+                            lastTweetMaxId = status.getId();
+                        }
+                        // Populates the tweet object with the search result
+                        Tweet tw = new Tweet(status.getId(), (String) status.getUser().getName(),
+                                status.getUser().getFollowersCount(), status.getUser().getLocation(), text,
+                                status.getCreatedAt(), candidate, 0, status.getRetweetCount(), status.isRetweet(),
+                                status.getGeoLocation());
+                        queryResult.add(tw);
+                    }
+                }
 
-	    } catch (TwitterException e) {
-		if (e.exceededRateLimitation()) {
-		    // Catches if we have exceeded the 180 searches limit and shows how long we have
-		    // to wait to run a new query
-		    System.out.println("Rate of searchs exceeded. Please wait "
-			    + e.getRateLimitStatus().getSecondsUntilReset() + " seconds and try again.");
-		    System.exit(0);
-		}
-	    } catch (NullPointerException f) {
-		System.out.println("The search did not retrieve any results. Please try again.");
-	    }
-	}
-	System.out.println(queryResult.size());
-	return queryResult;
+            } catch (TwitterException e) {
+                if (e.exceededRateLimitation()) {
+                    // Catches if we have exceeded the 180 searches limit and shows how long we have
+                    // to wait to run a new query
+                    System.out.println("Search rate exceeded. Please wait "
+                            + e.getRateLimitStatus().getSecondsUntilReset() + " seconds and try again.");
+                    System.exit(0);
+                }
+            } catch (NullPointerException f) {
+                System.out.println("The search did not retrieve any results. Please try again.");
+            }
+        }
+        System.out.println(queryResult.size());
+        return queryResult;
 
     }
 
     /**
      * Calculates the date seven days prior to the date entered
      *
-     * @param year  year entered by the user
-     * @param month month entered by the user
-     * @param day   day entered by the user
+     * @param date string
      * @return a string in the format YYYY-MM-DD
      */
 
     public String sinceDate(String date) { // Set as public for JUnit test
-	int year = Integer.valueOf(date.substring(0, 4));
-	int month = Integer.valueOf(date.substring(5, 7));
-	int day = Integer.valueOf(date.substring(8, 10));
+        int year = Integer.valueOf(date.substring(0, 4));
+        int month = Integer.valueOf(date.substring(5, 7));
+        int day = Integer.valueOf(date.substring(8, 10));
 
-	day = day - 7; // Seven days prior to the date entered
-	if (day <= 0) {
-	    month--;
-	}
-	if (month <= 0) {
-	    year--;
-	    month = 12;
-	}
-	if ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
-		&& day <= 0) {
-	    day = day + 31;
-	}
-	if ((month == 4 || month == 6 || month == 9 || month == 11) && day <= 0) {
-	    day = day + 30;
-	}
+        day = day - 7; // Seven days prior to the date entered
+        if (day <= 0) {
+            month--;
+        }
+        if (month <= 0) {
+            year--;
+            month = 12;
+        }
+        if ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+                && day <= 0) {
+            day = day + 31;
+        }
+        if ((month == 4 || month == 6 || month == 9 || month == 11) && day <= 0) {
+            day = day + 30;
+        }
 
-	if (month == 2 && day <= 0 && year == 2019) {
-	    day = day + 28;
-	}
-	if (month == 2 && day <= 0 && year == 2020) {
-	    day = day + 29;
-	}
+        if (month == 2 && day <= 0 && year == 2019) {
+            day = day + 28;
+        }
+        if (month == 2 && day <= 0 && year == 2020) {
+            day = day + 29;
+        }
 
-	return year + "-" + (month <= 10 ? "0" + month : month) + "-" + (day < 10 ? "0" + day : day);
+        return year + "-" + (month <= 10 ? "0" + month : month) + "-" + (day < 10 ? "0" + day : day);
     }
 
     /**
      * Calculates the date one day after the date entered
      *
-     * @param year  year entered by the user
-     * @param month month entered by the user
-     * @param day   day entered by the user
+     * @param date string
      * @return a string in the format YYYY-MM-DD
      */
     public String toDate(String date) { // Set as public for JUnit test
-	int year = Integer.valueOf(date.substring(0, 4));
-	int month = Integer.valueOf(date.substring(5, 7));
-	int day = Integer.valueOf(date.substring(8, 10));
+        int year = Integer.valueOf(date.substring(0, 4));
+        int month = Integer.valueOf(date.substring(5, 7));
+        int day = Integer.valueOf(date.substring(8, 10));
 
-	day = day + 1;// One day after entered date
+        day = day + 1;// One day after entered date
 
-	if ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
-		&& day > 31) {
-	    day = day - 31;
-	    month++;
-	}
-	if ((month == 4 || month == 6 || month == 9 || month == 11) && day > 30) {
-	    day = day - 30;
-	    month++;
-	}
+        if ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+                && day > 31) {
+            day = day - 31;
+            month++;
+        }
+        if ((month == 4 || month == 6 || month == 9 || month == 11) && day > 30) {
+            day = day - 30;
+            month++;
+        }
 
-	if (month == 2 && day >= 29 && year == 2019) {
-	    day = day - 28;
-	    month++;
-	}
-	if (month == 2 && day >= 30 && year == 2020) {
-	    day = day - 29;
-	    month++;
-	}
+        if (month == 2 && day >= 29 && year == 2019) {
+            day = day - 28;
+            month++;
+        }
+        if (month == 2 && day >= 30 && year == 2020) {
+            day = day - 29;
+            month++;
+        }
 
-	if (month >= 13) {
-	    year = year + 1;
-	    month = 1;
-	}
+        if (month >= 13) {
+            year = year + 1;
+            month = 1;
+        }
 
-	return year + "-" + (month < 10 ? "0" + month : month) + "-" + (day < 10 ? "0" + day : day);
+        return year + "-" + (month < 10 ? "0" + month : month) + "-" + (day < 10 ? "0" + day : day);
     }
 }
